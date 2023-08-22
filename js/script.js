f2affbdfbf6831e9bbe19a38d71bb688918eaaae
Miko <meirbekusenbek@gmail.com>
2023-08-22 13:17:14 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+const answers = [];
+
+answers[0] = prompt("What is your first name?");
+answers[1] = prompt("What is your second name?");
+answers[2] = prompt("How old are you?");
+
+document.write(answers);
+
+console.log(answers);
+
+const category = "toy";
+
+console.log(`https://google/${category}/5`)
